0093525d15be2e14fe5aab759df854bb50e6c73e
mubasharbinafzal391 <mubashar.afzal@selectasol.com>
2025-10-18 01:46:43 +0500

    Update api.ts

--- constants/api.ts
+++ constants/api.ts
@@ -4,4 +4,4 @@ const LOCAL_IP = "192.168.18.133"; // your Mac’s IP
 const ANDROID_LOCALHOST = "10.0.2.2";
 const DEFAULT_HOST = Platform.OS === "android" ? ANDROID_LOCALHOST : LOCAL_IP;
 
-export const API_URL = process.env.EXPO_PUBLIC_API_URL || `http://${`192.168.18.133`}:5000`;
+export const API_URL = process.env.EXPO_PUBLIC_API_URL || `http://${`10.0.2.2`}:5000`;
